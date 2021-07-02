4630aa880a99f3dc2af0580b4e6801a4d5d3566a
LuanLuizLLs <luanluiz.dev@gmail.com>
2021-07-02 18:17:29 -0300

    Melhorias diversas

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@ document.getElementById('botao_menu_mobile').onclick = function() {
     var menu_mobile  = document.getElementById('manual_menu');
     var menu_view = menu_mobile.getAttribute('view');
     if(menu_view == 'false') {
-        menu_mobile.style = "animation: mostrar 0.5s; z-index: 2;";
+        menu_mobile.style = "animation: mostrar-menu 0.5s; z-index: 2;";
         menu_mobile.setAttribute('view','true');
     } else {
-        menu_mobile.style = "animation: fechar 0.5s";
+        menu_mobile.style = "animation: fechar-menu 0.5s;";
         menu_mobile.setAttribute('view','false');
     }
 };
@@ -13,11 +13,20 @@ document.getElementById('botao_menu_mobile').onclick = function() {
 function FecharMenu() {
     var menu_mobile  = document.getElementById('manual_menu');
     var menu_view = menu_mobile.getAttribute('view');
-    if(menu_view == 'false') {
-        menu_mobile.style = "animation: mostrar 1s; z-index: 2;";
-        menu_mobile.setAttribute('view','true');
-    } else {
-        menu_mobile.style = "animation: fechar 1s";
+    if(menu_view == 'true') {
+        menu_mobile.style = "animation: fechar-menu 1s";
         menu_mobile.setAttribute('view','false');
+    }
+}
+
+function Descricao() {
+    var manual_descricao  = document.getElementById('manual_col_descricao');
+    var descricao_view = manual_descricao.getAttribute('view');
+    if(descricao_view == 'false') {
+        manual_descricao.style = "animation: ver-descricao 0.2s; width: 30%";
+        manual_descricao.setAttribute('view','true');
+    } else {
+        manual_descricao.style = "animation: fechar-descricao 0.2s;";
+        manual_descricao.setAttribute('view','false');
     }
 }
